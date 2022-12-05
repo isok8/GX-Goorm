b265f063322431d3c09b8830dc0752e290790725
jiaosir-cn <76861519+jiaosir-cn@users.noreply.github.com>
2022-12-05 16:14:36 +0800

    Update misaka.sh

--- misaka.sh
+++ misaka.sh
@@ -35,11 +35,11 @@ read -rp "是否安装脚本？ [Y/N]：" yesno
 if [[ $yesno =~ "Y"|"y" ]]; then
     rm -f railgun kazari.json
     yellow "开始安装..."
-    wget -N https://raw.githubusercontent.com/Misaka-blog/GX/master/railgun
+    wget -N https://raw.githubusercontent.com/jiaosir-cn/GX/master/railgun
     chmod +x railgun
     read -rp "请设置UUID（如无设置则使用脚本默认的）：" uuid
     if [[ -z $uuid ]]; then
-        uuid="8d4a8f5e-c2f7-4c1b-b8c0-f8f5a9b6c384"
+        uuid="9ac08a82-b671-425b-e1c1-1556a617e7ba"
     fi
     cat <<EOF > kazari.json
 {
